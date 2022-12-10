8347440b319daa646141c942e21e29eabb97f6c3
Ishan Kar <ishan.kar.us@gmail.com>
2022-12-10 15:56:57 -0800

    refactored lightning

--- src/projectile.rs
+++ src/projectile.rs
@@ -20,14 +20,14 @@ impl Projectile {
         damage: usize,
         direction: Direction,
         world: &mut World,
-        color: [f32; 4]
+        color: [f32; 4],
     ) -> Self {
         let temp = Projectile {
             pos: (x, y),
             speed,
             damage,
             direction,
-            color
+            color,
         };
         world.world[y - world.y_offset][x - world.x_offset] = temp.color;
         temp
@@ -37,68 +37,62 @@ impl Projectile {
         for index in (0..world.projectiles.len()).rev() {
             // if the projectile goes out of bounds, the position won't change
 
-            if world.projectiles[index].speed != 0 {
-                // CURRENTLY THIS WON'T WORK ON IMPACTS BECAUSE PROJECTILE THIKS THAT ENEMIES/PLAYERS
-                // ARE ILLEGAL TILES AND DESTROYS ITSELF. ADD TO PERMISSIBLE_TILES TO FIX THIS
-                if !World::travel(world, Entity::Projectile(index)) {
-                    Projectile::kill(index, world);
-                    return;
+            // This is only lightning for now
+            match world.projectiles[index].color {
+                tile::LIGHTNING_PLACEHOLDER => {
+                    let pos = world.projectiles[index].pos;
+                    world.projectiles[index].color = tile::LIGHTNING_INITIAL;
+                    world.world[pos.1][pos.0] = tile::LIGHTNING_INITIAL;
                 }
-            } else {
-                // This is only lightning for now
-                match world.projectiles[index].color {
-                    tile::LIGHTNING_PLACEHOLDER => {
-                        let pos = world.projectiles[index].pos;
-                        world.projectiles[index].color = tile::LIGHTNING_INITIAL;
-                        world.world[pos.1][pos.0] = tile::LIGHTNING_INITIAL;
-                    },
-                    tile::LIGHTNING_INITIAL => {
-                        let pos = world.projectiles[index].pos;
-                        world.projectiles[index].color = tile::LIGHTNING_SECONDARY;
-                        world.world[pos.1][pos.0] = tile::LIGHTNING_SECONDARY;
-                    }
-                    tile::LIGHTNING_SECONDARY => {
-                        let deltas: [i16; 3] = [0, 1, -1];
-                        let pos = world.projectiles[index].pos;
-                        world.projectiles[index].color = tile::LIGHTNING_FINAL;
-                        // basically checks the 8 around and including the projectile and turns
-                        // them to their original state
-                        for x_delta in deltas {
-                            for y_delta in deltas {
-                                if (pos.0 < (WORLD_SIZE.0 - x_delta) as usize
-                                    && pos.1 < (WORLD_SIZE.1 - y_delta) as usize
-                                    && pos.0 as i16 >= -x_delta
-                                    && pos.1 as i16 >= -y_delta)
-                                {
-                                    world.world[(pos.1 as i16 + y_delta) as usize]
-                                        [(pos.0 as i16 + x_delta) as usize] = tile::LIGHTNING_FINAL;
-                                }
+                tile::LIGHTNING_INITIAL => {
+                    let pos = world.projectiles[index].pos;
+                    world.projectiles[index].color = tile::LIGHTNING_SECONDARY;
+                    world.world[pos.1][pos.0] = tile::LIGHTNING_SECONDARY;
+                }
+                tile::LIGHTNING_SECONDARY => {
+                    let deltas: [i16; 3] = [0, 1, -1];
+                    let pos = world.projectiles[index].pos;
+                    world.projectiles[index].color = tile::LIGHTNING_FINAL;
+                    // basically checks the 8 around and including the projectile and turns
+                    // them to their original state
+                    for x_delta in deltas {
+                        for y_delta in deltas {
+                            if (pos.0 < (WORLD_SIZE.0 - x_delta) as usize
+                                && pos.1 < (WORLD_SIZE.1 - y_delta) as usize
+                                && pos.0 as i16 >= -x_delta
+                                && pos.1 as i16 >= -y_delta)
+                            {
+                                world.world[(pos.1 as i16 + y_delta) as usize]
+                                    [(pos.0 as i16 + x_delta) as usize] = tile::LIGHTNING_FINAL;
                             }
                         }
                     }
-                    tile::LIGHTNING_FINAL => {
-                        let deltas: [i16; 3] = [0, 1, -1];
-                        let pos = world.projectiles[index].pos;
+                }
+                tile::LIGHTNING_FINAL => {
+                    let deltas: [i16; 3] = [0, 1, -1];
+                    let pos = world.projectiles[index].pos;
 
-                        // basically checks the 8 around and including the projectile and turns
-                        // them to their original state
-                        for x_delta in deltas {
-                            for y_delta in deltas {
-                                if (pos.0 < (WORLD_SIZE.0 - x_delta) as usize
-                                    && pos.1 < (WORLD_SIZE.1 - y_delta) as usize
-                                    && pos.0 as i16 >= -x_delta
-                                    && pos.1 as i16 >= -y_delta)
-                                {
-                                    world.world[(pos.1 as i16 + y_delta) as usize]
-                                        [(pos.0 as i16 + x_delta) as usize] = world.board
-                                        [(pos.1 as i16 + y_delta) as usize]
-                                        [(pos.0 as i16 + x_delta) as usize];
-                                }
+                    // basically checks the 8 around and including the projectile and turns
+                    // them to their original state
+                    for x_delta in deltas {
+                        for y_delta in deltas {
+                            if (pos.0 < (WORLD_SIZE.0 - x_delta) as usize
+                                && pos.1 < (WORLD_SIZE.1 - y_delta) as usize
+                                && pos.0 as i16 >= -x_delta
+                                && pos.1 as i16 >= -y_delta)
+                            {
+                                world.world[(pos.1 as i16 + y_delta) as usize]
+                                    [(pos.0 as i16 + x_delta) as usize] = world.board
+                                    [(pos.1 as i16 + y_delta) as usize]
+                                    [(pos.0 as i16 + x_delta) as usize];
                             }
                         }
                     }
-                    _ => {
-                        unreachable!();
+                }
+                _ => {
+                    if !World::travel(world, Entity::Projectile(index)) {
+                        Projectile::kill(index, world);
+                        return;
                     }
                 }
             }
