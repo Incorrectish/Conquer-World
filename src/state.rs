9750c075650fdea7786d36871502fb147d3355d7
Ishan Kar <ishan.kar.us@gmail.com>
2022-12-13 09:52:14 -0800

    optimized draw to only draw on valid key events

--- src/state.rs
+++ src/state.rs
@@ -1,9 +1,9 @@
-use crate::UNIVERSAL_OFFSET;
 use crate::direction::Direction;
 use crate::enemy::Enemy;
 use crate::player::Player;
-use crate::utils::Position;
 use crate::utils::Boss;
+use crate::utils::Position;
+use crate::UNIVERSAL_OFFSET;
 use crate::{
     projectile::Projectile,
     tile,
@@ -19,11 +19,7 @@ use ggez::{
 use rand::rngs::ThreadRng;
 
 pub struct State {
-    // RGBA values
-    r: f32,
-    g: f32,
-    b: f32,
-    a: f32,
+    should_draw: bool,
 
     // Abstraction for the world and what is contained within it
     world: World,
@@ -33,10 +29,7 @@ impl State {
     // just returns the default values
     pub fn new() -> Self {
         Self {
-            r: 0.,
-            g: 0.,
-            b: 0.,
-            a: 0.,
+            should_draw: true,
             world: World::new(),
         }
     }
@@ -50,38 +43,40 @@ impl ggez::event::EventHandler<GameError> for State {
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         // render the graphics with the rgb value, it will always be black though because the
         // self.r,g,b,a values never change from 0
-        let mut canvas;
-        let mut boss_room = false;
-        for boss_room_position in BOSS_ROOMS {
-            if self.world.world_position == boss_room_position {
-                boss_room = true;
+        if self.should_draw {
+            let mut canvas;
+            let mut boss_room = false;
+            for boss_room_position in BOSS_ROOMS {
+                if self.world.world_position == boss_room_position {
+                    boss_room = true;
+                }
             }
+            if boss_room {
+                canvas = graphics::Canvas::from_frame(ctx, graphics::Color::from(tile::FLOOR));
+            } else {
+                canvas = graphics::Canvas::from_frame(ctx, graphics::Color::from(tile::GRASS));
+            }
+            self.world.draw(&mut canvas);
+
+            //For Text
+            // let level_dest = bevy::math::Vec2::new(10.0, 10.0);
+            // let score_dest = bevy::math::Vec2::new(200.0, 10.0);
+
+            // let level_str = format!("Level: 59");
+            // let score_str = format!("Score: 23423");
+
+            // canvas.draw(
+            //     &graphics::Text::new(level_str),
+            //     graphics::DrawParam::from(level_dest).color(tile::PORTAL),
+            // );
+
+            // canvas.draw(
+            //     &graphics::Text::new(score_str),
+            //     graphics::DrawParam::from(score_dest).color(tile::PORTAL),
+            // );
+            canvas.finish(ctx)?;
+            self.should_draw = false;
         }
-        if boss_room {
-            canvas = graphics::Canvas::from_frame(ctx, graphics::Color::from(tile::FLOOR));
-        } else {
-            canvas = graphics::Canvas::from_frame(ctx, graphics::Color::from(tile::GRASS));
-        }
-        self.world.draw(&mut canvas);
-
-        //For Text
-        // let level_dest = bevy::math::Vec2::new(10.0, 10.0);
-        // let score_dest = bevy::math::Vec2::new(200.0, 10.0);
-
-        // let level_str = format!("Level: 59");
-        // let score_str = format!("Score: 23423");
-
-        // canvas.draw(
-        //     &graphics::Text::new(level_str),
-        //     graphics::DrawParam::from(level_dest).color(tile::PORTAL),
-        // );
-
-        // canvas.draw(
-        //     &graphics::Text::new(score_str),
-        //     graphics::DrawParam::from(score_dest).color(tile::PORTAL),
-        // );
-        canvas.finish(ctx)?;
-        ggez::timer::yield_now();
         Ok(())
     }
 
@@ -99,6 +94,7 @@ impl ggez::event::EventHandler<GameError> for State {
             // less than or equal to 0
             Enemy::update(&mut self.world);
             Boss::update(&mut self.world);
+            self.should_draw = true;
         }
         Ok(())
     }
@@ -121,12 +117,12 @@ impl ggez::event::EventHandler<GameError> for State {
         _x: f32,
         _y: f32,
     ) -> Result<(), GameError> {
-        self.world.player.queued_position = Some(
-            Position::new(
+        if (_y / TILE_SIZE.1 as f32) as usize >= UNIVERSAL_OFFSET as usize {
+            self.world.player.queued_position = Some(Position::new(
                 (_x / TILE_SIZE.0 as f32) as usize,
                 (_y / TILE_SIZE.1 as f32) as usize - UNIVERSAL_OFFSET as usize,
-            ),
-        );
+            ));
+        }
         Ok(())
     }
 }
